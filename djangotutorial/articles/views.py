e4d524b464d8ad8a4ebb1f17a74fd85c0b487b80
BadreddineZatout <hb_zatout@esi.dz>
2021-12-17 15:26:34 +0100

    django course v1.2

--- djangotutorial/articles/views.py
+++ djangotutorial/articles/views.py
@@ -1,6 +1,8 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Article
+from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 def article_list(request):
@@ -8,8 +10,23 @@ def article_list(request):
     return render(request, 'articles/index.html', {
         'articles':articles
     })
+
 def article_detail(request, slug):
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/show.html', {
         'article': article
     })
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            #save article to db
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/create.html', {'form': form})
