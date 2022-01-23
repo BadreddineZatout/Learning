46280012dbca2e9cacd521fa8c4142e3e50ed99d
BadreddineZatout <hb_zatout@esi.dz>
2022-01-23 16:07:39 +0100

    vue3-course-v2.0

--- vue-tutorial/src/App.vue
+++ vue-tutorial/src/App.vue
@@ -1,56 +1,14 @@
 <template>
-  <h2>Volume Tracker (0-20)</h2>
-  <h2>Current Volume - {{ volume }}</h2>
-  <div>
-    <button @click="volume += 2">Increase</button>
-    <button @click="volume -= 2">Decrease</button>
-  </div>
-  <div>
-    <input type="text" v-model="movie" />
-    <input type="text" v-model="movieInfo.title" />
-    <input type="text" v-model="movieInfo.actor" />
-  </div>
-  <button @click="movieList.push('superman')">add movie</button>
+  <Greet />
 </template>
 
 <script>
+import Greet from "./components/Greet.vue";
+
 export default {
   name: "App",
-  data() {
-    return {
-      volume: 0,
-      movie: "",
-      movieInfo: {
-        title: "",
-        actor: "",
-      },
-      movieList: ["Batman", "Spider-man"],
-    };
-  },
-  methods: {},
-  computed: {},
-  watch: {
-    volume(newValue, oldValue) {
-      if (newValue >= 16 && newValue > oldValue) alert("high volume");
-    },
-    movie: {
-      handler(newValue) {
-        console.log(newValue);
-      },
-      immediate: true,
-    },
-    movieInfo: {
-      handler(newValue) {
-        console.log(newValue.title, newValue.actor);
-      },
-      deep: true,
-    },
-    movieList: {
-      handler(newValue) {
-        console.log(newValue);
-      },
-      deep: true,
-    },
+  components: {
+    Greet,
   },
 };
 </script>
---
+++ vue-tutorial/src/components/Greet.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>Hello Badreddine</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Greetings'
+    }
+</script>
