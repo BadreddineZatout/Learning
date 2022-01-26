e860bf2db135026f1edc0ba21a6e2e2ae2493055
BadreddineZatout <hb_zatout@esi.dz>
2022-01-26 22:20:00 +0100

    vue3-course-v4.8

--- vue-tutorial/src/components/composition-api/Person.vue
+++ vue-tutorial/src/components/composition-api/Person.vue
@@ -1,21 +1,24 @@
 <template>
   <div>
-      <input type="text" v-model="fname">
-      <input type="text" v-model="lname">
+    <input type="text" v-model="fname" />
+    <input type="text" v-model="lname" />
   </div>
-  <PersonGreeting :firstname="fname" :lastname="lname" />
+  <PersonGreeting :firstname="fname" :lastname="lname" @callHeroes="callHeroes" />
 </template>
 
 <script>
 import { ref } from "vue";
-import PersonGreeting from './PersonGreeting.vue'
+import PersonGreeting from "./PersonGreeting.vue";
 export default {
-    components: {PersonGreeting},
+  components: { PersonGreeting },
   setup() {
     const fname = ref("");
     const lname = ref("");
+    function callHeroes(hero) {
+      alert(`calling ${hero}`);
+    }
 
-    return { fname, lname };
+    return { fname, lname, callHeroes };
   },
 };
 </script>
--- vue-tutorial/src/components/composition-api/PersonGreeting.vue
+++ vue-tutorial/src/components/composition-api/PersonGreeting.vue
@@ -1,18 +1,23 @@
 <template>
   <div>hello {{ fullname }}</div>
+  <button @click="sendEvent">call Heros</button>
 </template>
 
 <script>
 import { computed } from "vue";
 export default {
-  setup(props) {
+  setup(props, context) {
     const fullname = computed(() => {
       return `${props.firstname} ${props.lastname}`;
     });
+    const sendEvent = () => {
+      context.emit("callHeroes", fullname.value);
+    };
 
-    return { fullname };
+    return { fullname, sendEvent };
   },
   props: ["firstname", "lastname"],
+  emits: ["callHeroes"],
 };
 </script>
 
