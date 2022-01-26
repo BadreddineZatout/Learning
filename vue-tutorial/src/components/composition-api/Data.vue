741aa115ad463e74cbb43203504314fb46693a6a
BadreddineZatout <hb_zatout@esi.dz>
2022-01-26 13:46:45 +0100

    vue3-course-v4.2

--- vue-tutorial/src/components/composition-api/Data.vue
+++ vue-tutorial/src/components/composition-api/Data.vue
@@ -2,28 +2,27 @@
   <div>
     <h3>{{ greet }}</h3>
     <h3>{{ reactiveGreet }}</h3>
+    <h3>{{ firstname }} - {{ lastname }}</h3>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { ref, reactive, toRefs } from "vue";
 export default {
   name: "Data",
   setup() {
     const firstname = ref("badreddine");
     const lastname = ref("zatout");
     const greet = `hello ${firstname.value} ${lastname.value}`;
+    console.log(greet);
     const state = reactive({
       firstname: "badi",
       lastname: "zatout",
     });
     state.firstname = 'zakaria';
-    const reactiveGreet = `Hello ${state.firstname} ${state.lastname}`;
+    // const reactiveGreet = `Hello ${state.firstname} ${state.lastname}`;
 
-    return {
-      greet,
-      reactiveGreet
-    };
+    return toRefs(state)
   },
 };
 </script>
