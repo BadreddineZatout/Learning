df7f342ddfc1850db45301cad8f16f5de5733694
BadreddineZatout <hb_zatout@esi.dz>
2022-01-28 21:42:57 +0100

    composition-api-store-v2.0

--- composition-api-store/src/App.vue
+++ composition-api-store/src/App.vue
@@ -3,8 +3,18 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
+
+<script>
+import store from "@/store";
+import { provide } from "vue";
+export default {
+  setup() {
+    provide("store", store);
+  },
+};
+</script>
 
 <style>
 #app {
--- composition-api-store/src/views/Home.vue
+++ composition-api-store/src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="counter" :style="{ color: store.state.color }">
+      {{ store.state.counter }}
+    </div>
+    <div class="counter-squared">
+      {{ store.state.counter }}
+      <sup>2</sup> = {{ store.getters.counterSquared() }}
+    </div>
+    <div class="buttons">
+      <button @click="store.methods.decreaseCounter">-</button>
+      <button @click="store.methods.increaseCounter">+</button>
+    </div>
+    <div>
+      <input type="text" placeholder="enter color" v-model="color" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { computed, inject } from "vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  setup() {
+    const store = inject("store");
+    //can be replaced with v-model
+    const color = computed({
+      get() {
+        return store.state.color;
+      },
+      set(newValue) {
+        store.methods.setColor(newValue);
+      },
+    });
+
+    return {
+      store,
+      color,
+    };
+  },
+};
 </script>
+<style>
+div {
+  margin-bottom: 10px;
+}
+.counter {
+  font-size: 80px;
+}
+.buttons button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
