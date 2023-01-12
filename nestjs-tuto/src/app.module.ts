234cd6c47bdea2c2f4b63d17eab936942fb888d2
Badreddine Zatout <hb_zatout@esi.dz>
2023-01-12 17:41:21 +0100

    setup typeorm

--- nestjs-tuto/src/app.module.ts
+++ nestjs-tuto/src/app.module.ts
@@ -1,8 +1,22 @@
+import { User } from './entities/User';
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nestjs',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
