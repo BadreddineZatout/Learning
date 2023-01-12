e8b60cd9efb120ca3a2e4b80a39583afdd236ecc
Badreddine Zatout <hb_zatout@esi.dz>
2023-01-12 16:41:21 +0100

    middlewares

--- nestjs-tuto/src/users/users.module.ts
+++ nestjs-tuto/src/users/users.module.ts
@@ -1,9 +1,28 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UsersController } from './controllers/users/users.controller';
 import { UsersService } from './services/users/users.service';
+import { UsersMiddleware } from './middlewares/users/users.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UsersMiddleware).forRoutes(
+      {
+        path: 'users',
+        method: RequestMethod.GET,
+      },
+      {
+        path: 'users/:id',
+        method: RequestMethod.GET,
+      },
+    );
+  }
+}
